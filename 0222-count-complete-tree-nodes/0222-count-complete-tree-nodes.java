5e3651a3a3fd3381d835165f5c6b262405ec4dcb
sumiiii05 <727623bit040@mcet.in>
2025-10-23 10:29:16 +0530

    Time: 0 ms (100%), Space: 47.6 MB (78.83%) - LeetHub

---
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int countNodes(TreeNode root) {
+        return NullNode(root)-1;
+    }
+
+    public int NullNode(TreeNode root){
+        if(root==null)
+            return 1;
+        int left=NullNode(root.left);
+        int right=NullNode(root.right);
+        return left+right;
+    }
+}
