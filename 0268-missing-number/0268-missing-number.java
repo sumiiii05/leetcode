ed2424d5798d8fa3c9504b13410dc0fe9d753a72
sumiiii05 <727623bit040@mcet.in>
2026-01-03 19:45:39 +0530

    Time: 0 ms (100%), Space: 47.6 MB (15.56%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n=nums.length;
+        int sum=n*(n+1)/2;
+        int asum=0;
+        for(int i=0;i<nums.length;i++){
+            asum+=nums[i];
+        }
+        return sum-asum;
+    }
+}
